12e7933b01042191bafb35c93c20f731828f54f3
rostislaim <7rostyk@gmail.com>
2024-06-07 00:05:56 +0300

    modified:   Sign.html 	modified:   contacts.html 	modified:   css/index.css 	modified:   css/news.css 	modified:   css/top.css 	modified:   index.html 	deleted:    music.mp3 	modified:   myapp/server.js 	modified:   top.html

--- myapp/server.js
+++ myapp/server.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express(); // Створення нового екземпляру Express
+
+app.use(cors()); // Додавання middleware для обробки CORS-запитів
+app.use(bodyParser.json()); // Додавання middleware для обробки JSON-даних у запитах
 
 // Підключення до MongoDB
 mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected")) // Вивід повідомлення про успішне підключення до MongoDB
+  .catch(err => console.error("MongoDB connection error:", err)); // Вивід повідомлення про помилку підключення до MongoDB
 
 // Визначення схеми та моделі користувача
 const userSchema = new mongoose.Schema({
@@ -18,22 +19,22 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema); // Створення моделі користувача на основі схеми
 
 // Маршрут для обробки реєстрації
 app.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const newUser = new User({ username, password });
-    await newUser.save();
-    res.status(201).json({ message: "User registered successfully" });
+    const { username, password } = req.body; // Отримання ім'я користувача та пароль з тіла запиту
+    const newUser = new User({ username, password }); // Створення нового користувача з отриманими даними
+    await newUser.save(); // Збереження нового користувача у базі даних
+    res.status(201).json({ message: "User registered successfully" }); // Відправка відповіді про успішну реєстрацію
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error(err); // Вивід помилки в консоль
+    res.status(500).json({ error: "Internal Server Error" }); // Відправка відповіді про внутрішню помилку сервера
   }
 });
 
 // Запуск сервера
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000"); // Вивід повідомлення про запуск сервера на порту 3000
 });
